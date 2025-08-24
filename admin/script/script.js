3297e6a00026b9a4f94c360c16cca79ccd320a02
Enrique <enrique.corpus.5004@hotmail.com>
2025-08-24 01:33:02 -0500

    add udapte api rest and modulation js ,css , html files

---
+++ admin/script/script.js
@@ -0,0 +1,113 @@
+
+
+document.addEventListener('DOMContentLoaded', async() => {
+
+//varibles globales
+    //Data
+let data= [];
+let listaAdministradores = [];
+let listaEstudiantes = [];
+let listaDocentes = [];
+
+    //tablas
+let tablaEstudiante = document.getElementById("tablaEstudiantes");
+    //grafica panel
+const canvas = document.getElementById("grafica");
+let  contexto2D = canvas.getContext("2d");
+
+//creacion de sistema de informacion 
+    //link api respuesta completa 
+
+const url=  "https://68a35617c5a31eb7bb1ff133.mockapi.io/Academiaswbar400/usuarios";
+
+  // Funciones de Fetch
+
+  async function fetchData() {
+        const res = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+
+        data = await res.json();
+
+        console.log(data)
+
+
+        listaAdministradores = data[0].administradores;
+        console.log(listaAdministradores);
+
+        listaDocentes = data[0].docentes;
+        console.log(listaDocentes);
+
+        listaEstudiantes = data[0].estudiantes;
+        console.log(listaEstudiantes)
+    }   
+
+
+function estudiantesInscritos(){
+
+    try {
+     
+      listaEstudiantes.forEach((usuario, index) => {
+    const filaEstudiante = document.createElement("tr");
+
+        filaEstudiante.innerHTML = `
+          <td> ${index + 1}</td>
+          <td><img src="${usuario.foto}" width="30"></td>
+           <td>${usuario.nombre}</td>
+          <td>${usuario.identificacion}</td>
+          <td>${usuario.tipo}</td>
+          <td>${usuario.usuario}</td>
+        `;
+
+        tablaEstudiante.appendChild(filaEstudiante);
+      });
+    } catch (error) {
+      console.error("Error al traer usuarios:", error);
+    }
+  
+}
+
+//graficas
+
+function verGrafica(){
+
+    const inscripciones = [3, 5, 7, 15];
+    const meses = ["May", "Jun",  "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"];
+    const ancho = canvas.width;
+    const alto = canvas.height;
+    const grosorDeBarra = ancho / inscripciones.length - 5;
+
+    inscripciones.forEach((valor, i) => {
+      let  x = i * (grosorDeBarra + 5);
+      let  h = valor * 5;
+      let  y = alto - h;
+
+    contexto2D.fillStyle = "#777272ff";
+    contexto2D.fillRect(x, y, grosorDeBarra, h);
+
+    contexto2D.fillStyle = "#ffffffff";
+    contexto2D.font = "10px Arial";
+    contexto2D.fillText(meses[i], x + grosorDeBarra / 4, alto - 5);
+
+    contexto2D.fillStyle = "#f3c548ff";
+    contexto2D.fillText(valor, x + ancho / 10, y -5);
+  })
+}
+
+//algoritmo
+
+await fetchData();
+estudiantesInscritos()
+verGrafica()
+
+});
+
+
+
+
+
+
--- script/script.js
+++ script/script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', async() => {
 
 //varibles globales
     //Data
-let dataLogin = [];
+let data= []
 let listaAdministradores = [];
 let listaEstudiantes = [];
 let listaDocentes = [];
@@ -16,132 +16,101 @@ let inputTipoUsuario = document.getElementById("floatingSelect");
 let inputUsuario = document.getElementById("floatingInput");
 let inputContrasena = document.getElementById("floatingPassword");
 let botonIngresar = document.getElementById("ingresar");
-    //tablas
-let tablaEstudiante = document.getElementById("tablaEstudiantes");
-    //grafica panel
-const canvas = document.getElementById("grafica");
-let  contexto2D = canvas.getContext("2d");
 
 //creacion de sistema de informacion 
     //link api respuesta completa 
 
-const mockApiBase =  "https://68a35617c5a31eb7bb1ff133.mockapi.io/Academiaswbar400";
+const url=  "https://68a35617c5a31eb7bb1ff133.mockapi.io/Academiaswbar400/usuarios";
 
-  // --- Funciones de Fetch ---
-  async function fetchEstudiantes() {
-    const data = await fetch(`${mockApiBase}/estudiantes`);
-    return await data.json();
-  }
+  // Funciones de Fetch
 
-  async function fetchDocentes() {
-    const data = await fetch(`${mockApiBase}/docentes`);
-    return await data.json();
-  }
+  async function fetchData() {
+        const res = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
 
-  async function fetchAdministradores() {
-    const data = await fetch(`${mockApiBase}/administradores`);
-    return await data.json();
-  }
 
+        data = await res.json();
+
+        console.log(data)
+
+
+        listaAdministradores = data[0].administradores;
+        console.log(listaAdministradores);
+
+        listaDocentes = data[0].docentes;
+        console.log(listaDocentes);
+
+        listaEstudiantes = data[0].estudiantes;
+        console.log(listaEstudiantes)
+    }   
+  
   //login 
-async function fetchLogin(){
-
-
-const respuesta = await fetch("https://68a35617c5a31eb7bb1ff133.mockapi.io/Academiaswbar400/usuarios", {method: "GET",
-"headers": {
-    "Content-Type": "application/json"
-}
-});
-
-dataLogin = await respuesta.json();
-return dataLogin;
-};
-
 
 async function login(){
 
-    if (dataLogin.length === 0) {
-        await usuarios();
+    if (listaEstudiantes.length === 0) {
+      await cargarUsuarios();
     }
 
-perfil = inputTipoUsuario.value;
-let usuario = inputUsuario.value;
-let contrasena = inputContrasena.value;
+    perfil = inputTipoUsuario.value;
+    let usuario = inputUsuario.value;
+    let contrasena = inputContrasena.value;
 
 
- if (perfil === "1") {
+    if (perfil === "1") {
+      usuarioEncontrado = listaAdministradores.find(u => u.usuario === usuario && u.contrasena === contrasena);
     
-    usuarioEncontrado = listaAdministradores.find(u => (u.usuario === usuario) && (u.contrasena === contrasena) )
+        //validacion
+        if (usuarioEncontrado) {
+        console.log("credenciales incorrecta")
+        window.location.href = "../admin/panel.html";
+            console.log("ingreso exitoso")
+        }
+        else {
+        console.log("Credenciales incorrectas");
+        };
+    }
     
+    else if (perfil === "2") { 
+      usuarioEncontrado = listaDocentes.find(u => u.usuario === usuario && u.contrasena === contrasena);
+
+      //validacion
     if (usuarioEncontrado) {
     console.log("credenciales incorrecta")
     window.location.href = "../pages/panel.html";
         console.log("ingreso exitoso")
     }
+     else {
+      console.log("Credenciales incorrectas");
+    };
+
+
+    } 
+    
+    else if (perfil === "3") { 
+      usuarioEncontrado = listaEstudiantes.find(u => u.usuario === usuario && u.contrasena === contrasena);
+
+      //validacion
+    if (usuarioEncontrado) {
+    console.log("credenciales incorrecta")
+    window.location.href = "../pages/panel.html";
+        console.log("ingreso exitoso")
     }
+     else {
+      console.log("Credenciales incorrectas");
+    };
+    };
 
  }
 
-
-function estudiantesInscritos(){
-
-    try {
-     
-      listaEstudiantes.forEach((usuario, index) => {
-    const filaEstudiante = document.createElement("tr");
-
-        filaEstudiante.innerHTML = `
-          <td> ${index + 1}</td>
-          <td><img src="${usuario.foto}" width="30"></td>
-           <td>${usuario.nombre}</td>
-          <td>${usuario.identificacion}</td>
-          <td>${usuario.tipo}</td>
-          <td>${usuario.usuario}</td>
-        `;
-
-        tablaEstudiante.appendChild(filaEstudiante);
-      });
-    } catch (error) {
-      console.error("Error al traer usuarios:", error);
-    }
-  
-}
-
-//graficas
-
-function verGrafica(){
-
-    const inscripciones = [3, 5, 7, 15];
-    const meses = ["May", "Jun",  "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"];
-    const ancho = canvas.width;
-    const alto = canvas.height;
-    const grosorDeBarra = ancho / inscripciones.length - 5;
-
-    inscripciones.forEach((valor, i) => {
-      let  x = i * (grosorDeBarra + 5);
-      let  h = valor * 5;
-      let  y = alto - h;
-
-    contexto2D.fillStyle = "#777272ff";
-    contexto2D.fillRect(x, y, grosorDeBarra, h);
-
-    contexto2D.fillStyle = "#ffffffff";
-    contexto2D.font = "10px Arial";
-    contexto2D.fillText(meses[i], x + grosorDeBarra / 4, alto - 5);
-
-    contexto2D.fillStyle = "#f3c548ff";
-    contexto2D.fillText(valor, x + ancho / 10, y -5);
-  })
-}
-
-
 //algoritmo
 
-await usuarios();
-estudiantesInscritos()
-verGrafica()
+await fetchData();
 botonIngresar.addEventListener("click",login)
-
 
 });
 
